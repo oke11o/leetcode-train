6d59f058ba5c586ddeda1f3d3adc0bbee5b3b26c
Sergey Bevzenko <bevzenko.sergey@gmail.com>
2021-09-15 22:59:43 +0300

    1189. Maximum Number of Balloons (v2)

--- go-solutions/1189-maximum-number-of-balloons.go
+++ go-solutions/1189-maximum-number-of-balloons.go
@@ -1,6 +1,34 @@
 package go_solutions
 
 func maxNumberOfBalloons(text string) int {
+	var min = func(nn ...int) int {
+		ans := 1<<((32<<(^uint(0)>>63))-1) - 1
+		for _, n := range nn {
+			if ans > n {
+				ans = n
+			}
+		}
+		return ans
+	}
+	var cntB, cntA, cntL, cntO, cntN int
+	for _, v := range text {
+		switch v {
+		case 'b':
+			cntB++
+		case 'a':
+			cntA++
+		case 'l':
+			cntL++
+		case 'o':
+			cntO++
+		case 'n':
+			cntN++
+		}
+	}
+	return min(cntB, cntA, cntL/2, cntO/2, cntN)
+}
+
+func maxNumberOfBalloons_2(text string) int {
 	assert := "balloon"
 	set := make(map[rune]int)
 	for _, v := range assert {
