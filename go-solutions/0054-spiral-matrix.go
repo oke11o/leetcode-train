7cd3f3c0ec3d9a2af47a294b5737afbb59d90bb1
Sergey Bevzenko <bevzenko.sergey@gmail.com>
2021-09-16 13:25:44 +0300

    spiralOrder refactoring

--- go-solutions/0054-spiral-matrix.go
+++ go-solutions/0054-spiral-matrix.go
@@ -2,43 +2,39 @@ package go_solutions
 
 func spiralOrder(matrix [][]int) []int {
 	result := make([]int, 0, len(matrix)*len(matrix[0]))
-	var (
-		up     = 0
-		bottom = len(matrix) - 1
-		left   = 0
-		right  = len(matrix[0]) - 1
-	)
+	direction := "left"
+	topEdge := 0
+	leftEdge := 0
+	bottomEdge := len(matrix) - 1
+	rightEdge := len(matrix[0]) - 1
 
-	direction := 0 // 0 - to left, 1 - to bottom; 2 - to right; 3 - to up
-
-	for up <= bottom && left <= right {
+	for topEdge <= bottomEdge && leftEdge <= rightEdge {
 		switch direction {
-		case 0:
-			for i := left; i <= right; i++ {
-				result = append(result, matrix[up][i])
+		case "left":
+			for i := leftEdge; i <= rightEdge; i++ {
+				result = append(result, matrix[topEdge][i])
 			}
-			up++
-			direction = 1
-		case 1:
-			for i := up; i <= bottom; i++ {
-				result = append(result, matrix[i][right])
+			topEdge++
+			direction = "down"
+		case "down":
+			for i := topEdge; i <= bottomEdge; i++ {
+				result = append(result, matrix[i][rightEdge])
 			}
-			right--
-			direction = 2
-		case 2:
-			for i := right; i >= left; i-- {
-				result = append(result, matrix[bottom][i])
+			rightEdge--
+			direction = "right"
+		case "right":
+			for i := rightEdge; i >= leftEdge; i-- {
+				result = append(result, matrix[bottomEdge][i])
 			}
-			bottom--
-			direction = 3
-		case 3:
-			for i := bottom; i >= up; i-- {
-				result = append(result, matrix[i][left])
+			bottomEdge--
+			direction = "up"
+		case "up":
+			for i := bottomEdge; i >= topEdge; i-- {
+				result = append(result, matrix[i][leftEdge])
 			}
-			left++
-			direction = 0
+			leftEdge++
+			direction = "left"
 		}
 	}
-
 	return result
 }
