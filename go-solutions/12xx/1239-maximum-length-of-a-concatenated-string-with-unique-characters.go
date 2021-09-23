781262e3d2aaa7cf52c0d4cd59660b6770c6a0ba
Sergey Bevzenko <bevzenko.sergey@gmail.com>
2021-09-23 20:20:26 +0300

    1239. Maximum Length of a Concatenated String with Unique Characters

--- go-solutions/12xx/1239-maximum-length-of-a-concatenated-string-with-unique-characters.go
+++ go-solutions/12xx/1239-maximum-length-of-a-concatenated-string-with-unique-characters.go
@@ -1,12 +1,60 @@
 package _2xx
 
 func maxLength(arr []string) int {
-	m := make(map[int32][]int)
-	for i, word := range arr {
-		for _, symbol := range word {
-			m[symbol] = append(m[symbol], i)
+
+	binWords := make([]int, 0, len(arr))
+	for i := 0; i < len(arr); i++ {
+		binWord, dubExists := maxLength_binWords(arr[i])
+		if !dubExists {
+			binWords = append(binWords, binWord)
 		}
 	}
 
-	return 0
+	result := 0
+	stack := []int{0}
+	resLen := len(stack)
+	for i := 0; i < resLen; i++ {
+		inRes := stack[i]
+		for _, binWord := range binWords {
+			isUniq := inRes & binWord
+			if isUniq != 0 {
+				continue
+			}
+			newWord := inRes | binWord
+			stack = append(stack, newWord)
+			resLen = len(stack)
+			binCnt := maxLength_countBits(newWord)
+			if result < binCnt {
+				result = binCnt
+			}
+		}
+	}
+
+	return result
+}
+
+func maxLength_binWords(word string) (int, bool) {
+	result := 0
+	hasDubs := false
+	for _, sym := range word {
+		sym = sym - 'a'
+		t := 1 << sym
+		dub := result & t
+		if dub > 0 {
+			hasDubs = true
+		}
+		result |= t
+	}
+	return result, hasDubs
+}
+
+func maxLength_countBits(n int) int {
+	result := 0
+	for n != 0 {
+		if n%2 == 1 {
+			result++
+		}
+		n = n / 2
+	}
+	return result
 }
--- go-solutions/12xx/1239-maximum-length-of-a-concatenated-string-with-unique-characters_test.go
+++ go-solutions/12xx/1239-maximum-length-of-a-concatenated-string-with-unique-characters_test.go
@@ -6,8 +6,6 @@ import (
 )
 
 func Test_maxLength(t *testing.T) {
-	type args struct {
-	}
 	tests := []struct {
 		name        string
 		arr         []string
@@ -23,7 +21,7 @@ func Test_maxLength(t *testing.T) {
 		{
 			name: "",
 			arr:  []string{"un", "iqe", "ueabc"},
-			want: 4,
+			want: 5,
 		},
 		{
 			name:        "",
@@ -40,6 +38,74 @@ func Test_maxLength(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := maxLength(tt.arr)
+			require.Equal(t, tt.want, got)
+		})
+	}
+}
+
+func Test_maxLength_binWords(t *testing.T) {
+	tests := []struct {
+		name  string
+		word  string
+		want  int
+		want2 bool
+	}{
+		{
+			name:  "",
+			word:  "abcd",
+			want:  15,
+			want2: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, got2 := maxLength_binWords(tt.word)
+			require.Equal(t, tt.want, got)
+			require.Equal(t, tt.want2, got2)
+		})
+	}
+}
+
+func Test_maxLength_countBits(t *testing.T) {
+	tests := []struct {
+		name string
+		num  int
+		want int
+	}{
+		{
+			name: "",
+			num:  1,
+			want: 1,
+		},
+		{
+			name: "",
+			num:  2,
+			want: 1,
+		},
+		{
+			name: "",
+			num:  3,
+			want: 2,
+		},
+		{
+			name: "",
+			num:  4,
+			want: 1,
+		},
+		{
+			name: "",
+			num:  7,
+			want: 3,
+		},
+		{
+			name: "",
+			num:  79,
+			want: 5,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := maxLength_countBits(tt.num)
 			require.Equal(t, tt.want, got)
 		})
 	}
