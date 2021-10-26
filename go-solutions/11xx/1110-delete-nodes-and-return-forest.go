f03647ec9f7a3ddd0517694635455088f55869c3
Sergey Bevzenko <bevzenko.sergey@gmail.com>
2021-10-26 22:12:06 +0300

    1110. Delete Nodes And Return Forest (ver2); refactoring

--- go-solutions/11xx/1110-delete-nodes-and-return-forest.go
+++ go-solutions/11xx/1110-delete-nodes-and-return-forest.go
@@ -2,38 +2,33 @@ package _1xx
 
 // 1110. Delete Nodes And Return Forest
 func delNodes(root *TreeNode, toDelete []int) []*TreeNode {
-	result := []*TreeNode{}
+	var result []*TreeNode
 	mapa := make(map[int]struct{}, len(toDelete))
 	for _, v := range toDelete {
 		mapa[v] = struct{}{}
 	}
 	queue := []*TreeNode{root}
-	result = append(result, root)
-
-	// Эти две мапы нужны для удаления нод из результата
-	inResutValues := map[int]int{root.Val: 0}
-	toSkipIdxs := map[int]struct{}{}
+	if _, ok := mapa[root.Val]; !ok {
+		result = append(result, root)
+	}
 
 	for len(queue) > 0 {
 		node := queue[0]
 		queue = queue[1:]
 		if _, ok := mapa[node.Val]; ok {
-			// так как этого ребенка добавили в результат чуть ниже, то тут делаем проверку, что может его все таки стоит удалить?
-			if idx, ok2 := inResutValues[node.Val]; ok2 {
-				toSkipIdxs[idx] = struct{}{}
-			}
 			if node.Left != nil {
-				result = append(result, node.Left)
-				inResutValues[node.Left.Val] = len(result) - 1
+				if _, ok := mapa[node.Left.Val]; !ok {
+					result = append(result, node.Left)
+				}
 			}
 			if node.Right != nil {
-				result = append(result, node.Right)
-				inResutValues[node.Right.Val] = len(result) - 1
+				if _, ok := mapa[node.Right.Val]; !ok {
+					result = append(result, node.Right)
+				}
 			}
 		}
 
-		// тут мы добавляем детей в обход и удаляем из детей текущей
-		// в результат детей добавляем выше
+		// Детей надо в любом случае добавить в обход
 		if node.Left != nil {
 			queue = append(queue, node.Left)
 			if _, ok := mapa[node.Left.Val]; ok {
@@ -47,15 +42,8 @@ func delNodes(root *TreeNode, toDelete []int) []*TreeNode {
 			}
 		}
 	}
-	if len(toSkipIdxs) == 0 {
-		return result
-	}
-	res := make([]*TreeNode, 0, len(result)-len(toSkipIdxs))
-	for i, v := range result {
-		if _, ok := toSkipIdxs[i]; !ok {
-			res = append(res, v)
-		}
-	}
 
-	return res
+	return result
 }
+
+// TODO: попробовать с dfs. Например: https://zhenyu0519.github.io/2020/03/21/lc1110/
