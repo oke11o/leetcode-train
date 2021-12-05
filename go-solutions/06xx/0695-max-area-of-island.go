ed4fa1d185cf19951e5ad437cd1be799fc9f92d2
Sergey Bevzenko <bevzenko.sergey@gmail.com>
2021-12-05 10:27:42 +0300

    0695. Max Area of Island (BFS)

--- go-solutions/06xx/0695-max-area-of-island.go
+++ go-solutions/06xx/0695-max-area-of-island.go
@@ -1,12 +1,16 @@
 package _6xx
 
-// 0695. Max Area of Island (DFS)
+// 0695. Max Area of Island (DFS, BFS)
 // Medium
 // Array, Matrix, BFS,DFS
 // https://leetcode.com/problems/max-area-of-island/
 func maxAreaOfIsland(grid [][]int) int {
 	result := newMaxAreaOfIslandVisited(len(grid), len(grid[0]))
-	return result.run(grid)
+	return result.calcMax_bfs(grid)
+}
+
+type maxAreaOfIslandResult struct {
+	visited [][]bool
 }
 
 func newMaxAreaOfIslandVisited(rowLen, colLen int) *maxAreaOfIslandResult {
@@ -19,11 +23,60 @@ func newMaxAreaOfIslandVisited(rowLen, colLen int) *maxAreaOfIslandResult {
 	return result
 }
 
-type maxAreaOfIslandResult struct {
-	visited [][]bool
+func (r *maxAreaOfIslandResult) calcMax_bfs(grid [][]int) int {
+	max := 0
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[0]); j++ {
+			if !r.visited[i][j] {
+				cnt := r.bfs(grid, i, j)
+				if max < cnt {
+					max = cnt
+				}
+			}
+		}
+	}
+	return max
 }
 
-func (r *maxAreaOfIslandResult) run(grid [][]int) int {
+func (r *maxAreaOfIslandResult) bfs(grid [][]int, i int, j int) int {
+	queue := make([][2]int, 0)
+	queue = append(queue, [2]int{i, j})
+
+	count := 0
+	for len(queue) > 0 {
+		pair := queue[0]
+		queue = queue[1:]
+
+		row := pair[0]
+		col := pair[1]
+		if r.visited[row][col] {
+			continue
+		}
+		r.visited[row][col] = true
+		if grid[row][col] == 0 {
+			continue
+		}
+		count++
+
+		if row >= 1 {
+			queue = append(queue, [2]int{row - 1, col})
+		}
+		if row < len(grid)-1 {
+			queue = append(queue, [2]int{row + 1, col})
+		}
+		if col >= 1 {
+			queue = append(queue, [2]int{row, col - 1})
+		}
+		if col < len(grid[0])-1 {
+			queue = append(queue, [2]int{row, col + 1})
+		}
+
+	}
+
+	return count
+}
+
+func (r *maxAreaOfIslandResult) calcMax_dfs(grid [][]int) int {
 	max := 0
 	for i := 0; i < len(grid); i++ {
 		for j := 0; j < len(grid[0]); j++ {
