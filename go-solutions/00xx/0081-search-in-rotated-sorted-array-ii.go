81fe0cb6d8c5e6c34baef4c7f2328aa6c2424d44
Sergey Bevzenko <bevzenko.sergey@gmail.com>
2021-12-28 08:46:31 +0300

    81. Search in Rotated Sorted Array II (TODO)

---
+++ go-solutions/00xx/0081-search-in-rotated-sorted-array-ii.go
@@ -0,0 +1,17 @@
+package _0xx
+
+/**
+ * 81. Search in Rotated Sorted Array II
+ * Medium
+ * #array, #binary_search
+ * https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+ *
+ * Reference:
+ * - 0153. Find Minimum in Rotated Sorted Array
+ * - 0033. Search in Rotated Sorted Array
+
+ * UNSOLVED, TODO
+ */
+func search81(nums []int, target int) bool {
+	return true
+}
