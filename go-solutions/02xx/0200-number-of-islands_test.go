a66c518f109b4661b1964f62a6a46f0e8d6e9187
Sergey Bevzenko <bevzenko.sergey@gmail.com>
2022-02-17 22:01:20 +0300

    200. Number of Islands (#union_find) with counter

--- go-solutions/02xx/0200-number-of-islands_test.go
+++ go-solutions/02xx/0200-number-of-islands_test.go
@@ -10,7 +10,7 @@ https://leetcode.com/problems/number-of-islands/
 func numIslands(grid [][]byte) int {
 	rowLen := len(grid)
 	colLen := len(grid[0])
-	uf := NewUnionFind(colLen * rowLen)
+	uf := NewUnionFind_numIslands(colLen * rowLen)
 	units := make(map[int]struct{}, colLen*rowLen)
 	for row := 0; row < rowLen; row++ {
 		for col := 0; col < colLen; col++ {
@@ -45,14 +45,13 @@ func numIslands(grid [][]byte) int {
 	return len(m)
 }
 
-type UnionFind struct {
-	unitCount int
-	rating    []int
-	parents   []int
+type UnionFind_numIslands struct {
+	rating  []int
+	parents []int
 }
 
-func NewUnionFind(n int) *UnionFind {
-	uf := &UnionFind{
+func NewUnionFind_numIslands(n int) *UnionFind_numIslands {
+	uf := &UnionFind_numIslands{
 		rating:  make([]int, n),
 		parents: make([]int, n),
 	}
@@ -63,7 +62,89 @@ func NewUnionFind(n int) *UnionFind {
 }
 
 // connect return flag. Is already connected
-func (uf *UnionFind) connect(a, b int) bool {
+func (uf *UnionFind_numIslands) connect(a, b int) bool {
+	parentA := uf.find(a)
+	parentB := uf.find(b)
+	if parentA == parentB {
+		return false
+	}
+	if uf.rating[parentA] == uf.rating[parentB] {
+		uf.parents[parentA] = parentB
+		uf.rating[parentB]++
+	} else if uf.rating[parentA] < uf.rating[parentB] {
+		uf.parents[parentA] = parentB
+	} else {
+		uf.parents[parentB] = parentA
+	}
+
+	return true
+}
+
+func (uf *UnionFind_numIslands) find(a int) int {
+	if uf.parents[a] == a {
+		return a
+	}
+	return uf.find(uf.parents[a])
+}
+
+/*********************************/
+/************* VERSION 2 *********/
+// Версия 2, сразу подсчитываем, кол-во единиц для union_find
+/*********************************/
+func numIslands_withCnt(grid [][]byte) int {
+	rowLen := len(grid)
+	colLen := len(grid[0])
+	uf := NewUnionFind_withIslandCnt_numIslands(grid)
+
+	for row := 0; row < rowLen; row++ {
+		for col := 0; col < colLen; col++ {
+			pos := row*colLen + col
+			if col+1 < colLen {
+				if grid[row][col] == '1' && grid[row][col] == grid[row][col+1] {
+					rightPos := row*colLen + col + 1
+					uf.connect(pos, rightPos)
+				}
+			}
+			if row+1 < rowLen {
+				if grid[row][col] == '1' && grid[row][col] == grid[row+1][col] {
+					downPos := (row+1)*colLen + col
+					uf.connect(pos, downPos)
+				}
+			}
+		}
+	}
+	return uf.unitCount
+}
+
+type UnionFind_withIslandCnt_numIslands struct {
+	unitCount int
+	rating    []int
+	parents   []int
+}
+
+func NewUnionFind_withIslandCnt_numIslands(grid [][]byte) *UnionFind_withIslandCnt_numIslands {
+	rowLen := len(grid)
+	colLen := len(grid[0])
+	uf := &UnionFind_withIslandCnt_numIslands{
+		rating:  make([]int, rowLen*colLen),
+		parents: make([]int, rowLen*colLen),
+	}
+	for row := 0; row < rowLen; row++ {
+		for col := 0; col < colLen; col++ {
+			pos := row*colLen + col
+			if grid[row][col] == '1' {
+				uf.parents[pos] = pos
+				uf.unitCount++
+			} else {
+				uf.parents[pos] = -1
+			}
+		}
+	}
+	return uf
+}
+
+// connect return flag. Is already connected
+func (uf *UnionFind_withIslandCnt_numIslands) connect(a, b int) bool {
 	parentA := uf.find(a)
 	parentB := uf.find(b)
 	if parentA == parentB {
@@ -82,7 +163,7 @@ func (uf *UnionFind) connect(a, b int) bool {
 	return true
 }
 
-func (uf *UnionFind) find(a int) int {
+func (uf *UnionFind_withIslandCnt_numIslands) find(a int) int {
 	if uf.parents[a] == a {
 		return a
 	}
@@ -170,9 +251,12 @@ func Test_numIslands(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := numIslands(tt.args.grid); got != tt.want {
+			if got := numIslands_withCnt(tt.args.grid); got != tt.want {
 				t.Errorf("numIslands() = %v, want %v", got, tt.want)
 			}
+			//if got := numIslands(tt.args.grid); got != tt.want {
+			//	t.Errorf("numIslands() = %v, want %v", got, tt.want)
+			//}
 		})
 	}
 }
