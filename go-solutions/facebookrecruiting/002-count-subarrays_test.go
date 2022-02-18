4a8158c5f6ece9d90775e34717fb420ed39f248e
Sergey Bevzenko <bevzenko.sergey@gmail.com>
2022-02-18 22:30:29 +0300

    meta: Contiguous Subarrays (brute force)

---
+++ go-solutions/facebookrecruiting/002-count-subarrays_test.go
@@ -0,0 +1,85 @@
+package facebookrecruiting
+
+import (
+	"reflect"
+	"testing"
+)
+
+/**
+Contiguous Subarrays
+You are given an array arr of N integers. For each index i, you are required to determine the number of contiguous subarrays that fulfill the following conditions:
+The value at index i must be the maximum element in the contiguous subarrays, and
+These contiguous subarrays must either start from or end on index i.
+Signature
+int[] countSubarrays(int[] arr)
+Input
+Array arr is a non-empty list of unique integers that range between 1 to 1,000,000,000
+Size N is between 1 and 1,000,000
+Output
+An array where each index i contains an integer denoting the maximum number of contiguous subarrays of arr[i]
+Example:
+arr = [3, 4, 1, 6, 2]
+output = [1, 3, 1, 5, 1]
+Explanation:
+For index 0 - [3] is the only contiguous subarray that starts (or ends) with 3, and the maximum value in this subarray is 3.
+For index 1 - [4], [3, 4], [4, 1]
+For index 2 - [1]
+For index 3 - [6], [6, 2], [1, 6], [4, 1, 6], [3, 4, 1, 6]
+For index 4 - [2]
+So, the answer for the above input is [1, 3, 1, 5, 1]
+*/
+// We don’t provide test cases in this language yet, but have outlined the signature for you. Please write your code below, and don’t forget to test edge cases!
+func countSubarrays(arr []int) []int {
+	result := make([]int, len(arr))
+	for i, v := range arr {
+		for j := i; j >= 0 && arr[j] <= v; j-- {
+			result[i]++
+		}
+		for j := i; j < len(arr) && arr[j] <= v; j++ {
+			result[i]++
+		}
+		result[i]--
+	}
+	return result
+}
+
+//3, 4, 1, 6, 2
+//   ^
+//1
+//0
+
+/*********************************/
+/************* TESTS *************/
+/*********************************/
+func Test_countSubarrays(t *testing.T) {
+	type args struct {
+		arr []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "",
+			args: args{
+				arr: []int{3, 4, 1, 6, 2},
+			},
+			want: []int{1, 3, 1, 5, 1},
+		},
+		{
+			name: "",
+			args: args{
+				arr: []int{2, 4, 7, 1, 5, 3},
+			},
+			want: []int{1, 2, 6, 1, 3, 1},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countSubarrays(tt.args.arr); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("countSubarrays() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
