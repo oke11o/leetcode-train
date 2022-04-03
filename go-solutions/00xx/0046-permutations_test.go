2b46be28a7b35cf426b20cd59740b47b4e617651
Sergey Bevzenko <bevzenko.sergey@gmail.com>
2022-04-03 10:15:03 +0300

    0046. Permutations (move)

--- go-solutions/00xx/0046-permutations_test.go
+++ go-solutions/00xx/0046-permutations_test.go
@@ -1,10 +1,41 @@
 package _0xx
 
 import (
-	"github.com/stretchr/testify/require"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
+var resultPermute [][]int
+
+/**
+ * 0046. Permutations
+ * Medium
+ * #backtracking
+ */
+func permute(nums []int) [][]int {
+	resultPermute = make([][]int, 0)
+	permuteBacktrack(0, nums)
+	return resultPermute
+}
+
+func permuteBacktrack(current int, nums []int) {
+	if current == len(nums) {
+		item := make([]int, len(nums))
+		copy(item, nums)
+		resultPermute = append(resultPermute, item)
+		return
+	}
+	for i := current; i < len(nums); i++ {
+		nums[i], nums[current] = nums[current], nums[i]
+		permuteBacktrack(current+1, nums)
+		nums[i], nums[current] = nums[current], nums[i]
+	}
+}
+
+/*********************************/
+/************* TESTS *************/
+/*********************************/
 func Test_permute(t *testing.T) {
 	tests := []struct {
 		name string
