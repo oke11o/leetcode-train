a3dc8039dd77cabd3ffc85531367cd352cf93362
Sergey Bevzenko <bevzenko.sergey@gmail.com>
2022-04-03 14:11:03 +0300

    346. Moving Average from Data Stream

--- go-solutions/03xx/0346-moving-average-from-data-stream.go
+++ go-solutions/03xx/0346-moving-average-from-data-stream.go
@@ -34,3 +34,35 @@ func (this *MovingAverage) Next(val int) float64 {
 	this.sum += val
 	return float64(this.sum) / float64(len(this.queue))
 }
+
+/**
+With cicle queue
+*/
+type MovingAverage_2 struct {
+	maxSize int
+	queue   []int
+	sum     int
+	pointer int
+}
+
+func Constructor_2(size int) MovingAverage_2 {
+	return MovingAverage_2{
+		maxSize: size,
+		queue:   make([]int, 0, size),
+	}
+}
+
+func (this *MovingAverage_2) Next(val int) float64 {
+	if len(this.queue) < this.maxSize {
+		this.sum += val
+		this.queue = append(this.queue, val)
+		this.pointer++
+		return float64(this.sum) / float64(len(this.queue))
+	}
+	this.pointer %= this.maxSize
+	this.sum -= this.queue[this.pointer]
+	this.queue[this.pointer] = val
+	this.pointer++
+	this.sum += val
+	return float64(this.sum) / float64(len(this.queue))
+}
