6c1283d691373f5c9e1fe75b490791025732fa15
Sergey Bevzenko <bevzenko.sergey@gmail.com>
2023-01-13 17:34:06 +0300

    0003. Longest Substring Without Repeating Characters (fix naming)

--- go-solutions/00xx/0003-longest-substring-without-repeating-characters.go
+++ go-solutions/00xx/0003-longest-substring-without-repeating-characters.go
@@ -9,26 +9,23 @@ func lengthOfLongestSubstring(input string) int {
 	if len(input) == 0 {
 		return 0
 	}
-	right := 0
+	left := 0
 	result := 0
 
 	district := make(map[uint8]int)
 	sum := 0
-	for left := 0; left < len(input); left++ {
+	for right := 0; right < len(input); right++ {
 		sum++
-		leftSym := input[left]
-		if _, ok := district[leftSym]; !ok {
-			district[leftSym] = 0
-		}
-		district[leftSym]++
+		rightLetter := input[right]
+		district[rightLetter]++
 		for sum > len(district) {
-			rightSym := input[right]
-			district[rightSym]--
-			if district[rightSym] == 0 {
-				delete(district, rightSym)
+			leftLetter := input[left]
+			district[leftLetter]--
+			if district[leftLetter] == 0 {
+				delete(district, leftLetter)
 			}
 			sum--
-			right++
+			left++
 		}
 
 		if result < sum {
